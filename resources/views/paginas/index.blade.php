64ead12f2d1946d48d49d2fe4752adb4cbe2a179
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-05-30 15:13:32 -0300

    Finalizado a estrutura da Home

--- resources/views/paginas/index.blade.php
+++ resources/views/paginas/index.blade.php
@@ -1,7 +1,7 @@
 @extends('template.menu_e_footer')
 
 @section('content')
-<div class="conteiner" style="width: 100%; margin: auto; margin-top: 10px; background-color:black;">
+<div class="conteiner" style="width: 100%; margin: auto; background-color:black;">
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -10,13 +10,25 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active" style="height:450px; width: 100%">
-        <img src="{{ asset('img/promissed_neverland.png') }}" class="d-block w-100;" alt="...">
+        <img id="homeImagemSlide1" src="{{ asset('img/promissed_neverland.png') }}" class="d-block w-100;">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 id="homeTituloSlide1"></h5>
+          <p id="homeTextoSlide1">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
       </div>
       <div class="carousel-item" style="height:450px; width: 100%;">
-        <img src="{{ asset('img/LogoAbrigo.png') }}" class="d-block w-100" alt="...">
+        <img id="homeImagemSlide2" src="{{ asset('img/LogoAbrigo.png') }}" class="d-block w-100">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 id="homeTituloSlide2">Second slide label</h5>
+          <p id="homeTextoSlide2">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
       </div>
       <div class="carousel-item" style="height:450px; width: 100%;">
-        <img src="{{ asset('img/overlord3.jpg') }}" class="d-block w-100" alt="...">
+        <img id="homeImagemSlide3" src="{{ asset('img/overlord3.jpg') }}" class="d-block w-100">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 id="homeTituloSlide3">Second slide label</h5>
+          <p id="homeTextoSlide3">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
       </div> 
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -29,13 +41,13 @@
     </a>
   </div>
 </div>
-<div style="margin: auto; width: 88%; margin-top: 10px">
+<div style="width: 88%; margin-top: 30px; margin-left: 10%">
   <div style="width: 45%; text-align: center; display: block; overflow: hidden; float: left">
     <h2>asdasdsad</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti saepe illum. Hic placeat corporis et cupiditate?</p>
+    <p style="text-align: justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut veniam odio minima quod perspiciatis deleniti accusantium omnis voluptas quaerat quia. Eos, molestiae voluptatum amet in quas dolorum obcaecati? Quisquam, possimus.</p>
   </div>
-  <div style="width: 35%; margin-left: 5%; display: block; overflow: hidden; float: right">
-    <img style="height:160px; width: 80%;" src="{{ asset('img/overlord3.jpg') }}" alt="...">
+  <div style="width: 35%; display: block; overflow: hidden; float: right">
+    <img id="homeImagemContribuidores" src="{{ asset('img/overlord3.jpg') }}" style="height:160px; width: 80%;">
   </div>
 </div>
 @endsection
