50a22661fd3783b5e135108cc7c4c46ed7413a69
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-06-05 13:51:25 -0300

    Estrututra da tela inicial pronta

--- resources/views/paginas/index.blade.php
+++ resources/views/paginas/index.blade.php
@@ -10,21 +10,21 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active" style="height:450px; width: 100%">
-        <img id="homeImagemSlide1" src="{{ asset('img/promissed_neverland.png') }}" class="d-block w-100;">
+        <img id="homeImagemSlide1" src="{{ asset('img/FundoPreto.jpg') }}" class="d-block w-100;">
         <div class="carousel-caption d-none d-md-block">
           <h5 id="homeTituloSlide1">First slide label</h5>
           <p id="homeTextoSlide1">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
       <div class="carousel-item" style="height:450px; width: 100%;">
-        <img id="homeImagemSlide2" src="{{ asset('img/LogoAbrigo.png') }}" class="d-block w-100">
+        <img id="homeImagemSlide2" src="{{ asset('img/FundoVermelho.jpg') }}" class="d-block w-100">
         <div class="carousel-caption d-none d-md-block">
           <h5 id="homeTituloSlide2">Second slide label</h5>
           <p id="homeTextoSlide2">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
       <div class="carousel-item" style="height:450px; width: 100%;">
-        <img id="homeImagemSlide3" src="{{ asset('img/overlord3.jpg') }}" class="d-block w-100">
+        <img id="homeImagemSlide3" src="{{ asset('img/FundoRosa.jpg') }}" class="d-block w-100">
         <div class="carousel-caption d-none d-md-block">
           <h5 id="homeTituloSlide3">Third slide label</h5>
           <p id="homeTextoSlide3">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -47,7 +47,7 @@
     <p style="text-align: justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut veniam odio minima quod perspiciatis deleniti accusantium omnis voluptas quaerat quia. Eos, molestiae voluptatum amet in quas dolorum obcaecati? Quisquam, possimus.</p>
   </div>
   <div style="width: 35%; display: block; overflow: hidden; float: right">
-    <img id="homeImagemContribuidores" src="{{ asset('img/overlord3.jpg') }}" style="height:160px; width: 80%;">
+    <img id="homeImagemContribuidores" src="{{ asset('img/FundoPreto.jpg') }}" style="height:160px; width: 80%;">
   </div>
 </div>
 @endsection
