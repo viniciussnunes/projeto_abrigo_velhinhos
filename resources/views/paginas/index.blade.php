f4be1f7404bf55593991cec2fde59debf36298ad
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-06-06 16:30:25 -0300

    Home completamente responsiva, consertado os bugs de responsividade, botoes adicionados aos cards

--- resources/views/paginas/index.blade.php
+++ resources/views/paginas/index.blade.php
@@ -10,21 +10,21 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active carrosselItem">
-        <img id="homeImagemSlide1" src="{{ asset('img/FundoPreto.jpg') }}" class="d-block w-100;">
+        <img id="homeImagemSlide1" class="imgCarrossel" src="{{ asset('img/FundoPreto.jpg') }}" class="d-block w-100;">
         <div class="carousel-caption d-none d-md-block">
           <h5 id="homeTituloSlide1">First slide label</h5>
           <p id="homeTextoSlide1">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
       <div class="carousel-item carrosselItem">
-        <img id="homeImagemSlide2" src="{{ asset('img/FundoVermelho.jpg') }}" class="d-block w-100">
+        <img id="homeImagemSlide2" class="imgCarrossel" src="{{ asset('img/FundoVermelho.jpg') }}" class="d-block w-100">
         <div class="carousel-caption d-none d-md-block">
           <h5 id="homeTituloSlide2">Second slide label</h5>
           <p id="homeTextoSlide2">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
       <div class="carousel-item carrosselItem">
-        <img id="homeImagemSlide3" src="{{ asset('img/FundoRosa.jpg') }}" class="d-block w-100">
+        <img id="homeImagemSlide3" class="imgCarrossel" src="{{ asset('img/FundoRosa.jpg') }}" class="d-block w-100">
         <div class="carousel-caption d-none d-md-block">
           <h5 id="homeTituloSlide3">Third slide label</h5>
           <p id="homeTextoSlide3">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -41,22 +41,22 @@
     </a>
   </div>
 </div>
-<div id="divParceiros">
-  <div id="divParceirosTexto">
+<div class="card-deck divParceiros">
+  <div class="card divParceirosTexto">
     <h2>Parceiros</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut veniam odio minima quod perspiciatis deleniti accusantium omnis voluptas quaerat quia. Eos, molestiae voluptatum amet in quas dolorum obcaecati? Quisquam, possimus.</p>
   </div>
-  <div id="divParceirosImagem">
+  <div class="card divParceirosImagem">
     <img id="homeImagemContribuidores " src="{{ asset('img/FundoPreto.jpg') }}">
   </div>
 </div>
-<div class="card-deck marginbottom cardHome" >
+<div class="card-deck cardHome" >
   <div class="card">
     <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <a href="#" class="btn btn-primary">Visitar</a>  
     </div>
   </div>
   <div class="card">
@@ -64,7 +64,7 @@
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <a href="#" class="btn btn-primary">Visitar</a>
     </div>
   </div>
   <div class="card">
@@ -72,7 +72,7 @@
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <a href="#" class="btn btn-primary">Visitar</a>    
     </div>
   </div>
 </div>
--- resources/views/template/menu_e_footer.blade.php
+++ resources/views/template/menu_e_footer.blade.php
@@ -11,13 +11,15 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light" >
-  <a class="navbar-brand" href="#"><img src = "LogoAbrigo.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 <div> 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto" >
+      <li class="nav-item" id="nav-item-logo">
+        <a class="navbar-brand" href="#"><img src="..."></a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="/home">Home</a>
       </li>
