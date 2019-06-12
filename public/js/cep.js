8c52f0120c83164328bc71b47ed51110a27434fc
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-06-12 15:38:55 -0300

    cadastro e o js para funcionar o cep dinamico, o cadastro ainda vai ser alterado pq foi modificado os campos

---
+++ public/js/cep.js
@@ -0,0 +1,66 @@
+//<!-- Adicionando Javascript -->
+
+    
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('resrua').value=("");
+    document.getElementById('rescidade').value=("");
+    document.getElementById('resbairro').value=("");
+
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById('resrua').value=(conteudo.logradouro);
+    document.getElementById('rescidade').value=(conteudo.localidade);
+    document.getElementById('resbairro').value=(conteudo.bairro);
+    
+} //end if.
+else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+//Nova variável "cep" somente com dígitos.
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+if (cep != "") {
+
+    //Expressão regular para validar o CEP.
+    var validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if(validacep.test(cep)) {
+
+        //Preenche os campos com "..." enquanto consulta webservice.
+        document.getElementById('resrua').value="...";
+        document.getElementById('rescidade').value=("...");
+        document.getElementById('resbairro').value=("...");
+        
+        //Cria um elemento javascript.
+        var script = document.createElement('script');
+
+        //Sincroniza com o callback.
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+        //Insere script no documento e carrega o conteúdo.
+        document.body.appendChild(script);
+
+    } //end if.
+    else {
+        //cep é inválido.
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+} //end if.
+else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+}
+};
--- resources/views/paginas/cadastros.blade.php
+++ resources/views/paginas/cadastros.blade.php
@@ -2,6 +2,134 @@
 
 @section('content')
 
+<main role="main" class="container">
+
+<div class="starter-template" style="margin-top: 100px;">
+  <h1>Cadastro</h1>
+  <p class="lead">Voluntarios.<br> Cadastro de Velhinhos e seu responsável.</p>
+</div>
+
+<form>
+  <div class="form-group">
+     <label for="inputAddress">Nome Completo</label>
+     <input type="text" class="form-control" name="volnome" placeholder="Nome">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">E-mail</label>
+    <input type="text" class="form-control" name="volemail" placeholder="E-mail">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Telefone</label>
+    <input type="text" class="form-control" name="voltelefone" placeholder="Telefone">
+  </div>
+  <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Função</label>
+        <input type="text" class="form-control" name="volfuncao" placeholder="Função">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPassword4">Tempo Hábil</label>
+        <select type="text" class="form-control" name="voltempo" placeholder="Tempo Hábil">
+          <option>...</option>
+          <option>Matutino</option>
+          <option>Vespertino</option>
+          <option>Integral</option>>
+        </select>
+      </div>
+    </div>
+  
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+<!-- ##############CADASTRO DE IDOSO##################################### -->
+<h2>Cadastro de Idosos!</h2>
+
+<form>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputEmail4">Nome Completo</label>
+          <input type="text" class="form-control" name="idnome" placeholder="Nome">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputPassword4">Doenças crônicas?</label>
+          <input type="text" class="form-control" name="iddoenca" placeholder="Descreva">
+        </div>
+      </div>
+      
+      <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputEmail4">Data de Nascimento</label>
+            <input type="date" class="form-control" name="idnasci" placeholder="Data">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputPassword4">Aposentado</label>
+            <select type="text" class="form-control" name="idaposentado" placeholder="Aposentado">
+              <option>...</option>
+              <option>Sim</option>
+              <option>Não</option>
+            </select>
+          </div>
+        </div>
+      <h3>Responsável</h3>
+      <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">Nome Completo</label>
+                <input type="text" class="form-control" name="resnome" placeholder="Nome">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPassword4">Telefone</label>
+                <input type="text" class="form-control" name="restelefone" placeholder="Telefone">
+              </div>
+            </div>
+
+            <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">Data de Nascimento</label>
+                    <input type="date" class="form-control" name="resdate" placeholder="Data de Nascimento">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="inputPassword4">E-mail</label>
+                    <input type="text" class="form-control" name="resemail" placeholder="E-mail">
+                  </div>
+                </div>
+
+                <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="inputEmail4">Grau de Parentesco</label>
+                        <input type="text" class="form-control" name="resparente" placeholder="Nome">
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="inputPassword4">CEP</label>
+                        <input type="number" class="form-control" name="rescep" placeholder="CEP" onblur="pesquisacep(this.value);">
+                      </div>
+                    </div>
+
+                    <div class="form-row">
+                          <div class="form-group col-md-6">
+                            <label for="inputEmail4">Número</label>
+                            <input type="number" class="form-control" name="resnumero" placeholder="Número">
+                          </div>
+                          <div class="form-group col-md-6">
+                            <label for="inputPassword4">Rua</label>
+                            <input type="text" class="form-control" name="resrua" id="resrua" placeholder="Rua">
+                          </div>
+                        </div>
+
+                        <div class="form-row">
+                              <div class="form-group col-md-6">
+                                <label for="inputEmail4">Bairro</label>
+                                <input type="text" class="form-control" name="resbairro" id="resbairro" placeholder="Bairro">
+                              </div>
+                              <div class="form-group col-md-6">
+                                <label for="inputPassword4">Cidade</label>
+                                <input type="text" class="form-control" name="rescidade" id="rescidade" placeholder="Cidade">
+                              </div>
+                            </div>
+
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+
+</main>
 
 
 @endsection
