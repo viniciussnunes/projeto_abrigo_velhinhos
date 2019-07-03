37b15d1c67fe099d2fc5e14780842ca9e8bd6689
sulserviceeng <sulservice.eng@gmail.com>
2019-07-03 17:10:55 -0300

    alteraçoes na pagina sobre

--- resources/views/paginas/sobre.blade.php
+++ resources/views/paginas/sobre.blade.php
@@ -3,5 +3,17 @@
 @section('content')
 
 
+        <div class="container" >
+            <div class="card text-white" style="margin-bottom:100px">
+                <img src="{{ asset('img/image.jpg') }}" class="img-sobre" alt="...">
+                <div class="card-img-overlay">
+                    <div style="width:400px;">
+                    <h5 class="card-title">Sobre</h5>
+                    <p class="card-text">"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is </p>
+                    <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 @endsection
