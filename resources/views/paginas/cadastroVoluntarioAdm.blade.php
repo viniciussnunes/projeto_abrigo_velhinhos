8f900bea86679a10d0bf329ba0793e2ef4fbe3c7
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-07-04 19:47:34 -0300

    As tabelas foram adicionadas as paginas de Registros

--- resources/views/paginas/cadastroVoluntarioAdm.blade.php
+++ resources/views/paginas/cadastroVoluntarioAdm.blade.php
@@ -2,5 +2,43 @@
 
 @section('contentAdm')
 
+<div class="container tabela">
+    <h1>Registro de Voluntários</h1>
+    <h6>Esses são os registros de todas as pessoas que desejam se voluntariar</h6>
+<table class="table table-dark tabela " style="border-radius:10px">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Nome Completo</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Telefone</th>
+      <th scope="col">Função</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Gilberto Robinhison</td>
+      <td>Otto@gmail</td>
+      <td>994762245</td>
+      <td>Faz nada</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob Cleitinho</td>
+      <td>Thornton@gmail</td>
+      <td>994762245</td>
+      <td>Faz nada</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry List</td>
+      <td>the Bird@gmail</td>
+      <td>994762245</td>
+      <td>Faz nada</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 
 @endsection
