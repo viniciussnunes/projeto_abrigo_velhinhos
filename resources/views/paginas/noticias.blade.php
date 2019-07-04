10bb64009b223cc5e8257ba3d3f84a46608ae049
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-07-04 18:04:03 -0300

    ta funcionando

--- resources/views/paginas/noticias.blade.php
+++ resources/views/paginas/noticias.blade.php
@@ -1,13 +1,13 @@
 @extends('template.menu_e_footer')
 
 @section('content')
-<div class="container ">   
-    <div class="media contnoticia">
-      <div class="media-body">
-        <h5 class="mt-0 mb-1 bbb">Media object</h5>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+  <div class="container div-noticias">   
+      <div class="media contnoticia">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">Media object</h5>
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p> 
+        </div>
+        <img width="500" height="auto" src="{{ asset('img/logo/abrigologo.png') }}"  alt="...">
       </div>
-      <img width="500" height="auto" src="{{ asset('img/logo/abrigologo.png') }}"  alt="...">
-    </div>
-</div>
+  </div>
 @endsection
