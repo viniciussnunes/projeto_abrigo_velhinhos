124b21e5188a23de122cbf5a514f661e0dea821e
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-07-11 20:15:48 -0300

    menuAdm modificado

--- resources/views/template/menuAdm.blade.php
+++ resources/views/template/menuAdm.blade.php
@@ -22,36 +22,43 @@
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.css" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js"></script>
+
+
+  <link rel="stylesheet" href="{{asset('/css/open-iconic-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/aos.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+    <title>Abrigo Dos Velhinhos</title>
+
 </head>
 
-<body class="body-branco">
+<body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="menu">
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item item-menu">
-            <a style="margin:0;" ; class="nav-link-meio" href="/homer">Voltar para o Site</a>
-          </li>
-          <li class="nav-item item-menu">
-            <a class="nav-link-meio" style="margin:0px 80px 0px 60px;" href="/publicacaoAdm">Fazer publicação</a>
-          </li>
-          <li class="nav-item item-menu" id="nav-item-logo">
-            <a href="/homer"><img class="logo" src="{{ asset('img/logo/abrigologo.png') }}"></a>
-          </li>
-          <li class="nav-item item-menu">
-            <a class="nav-link-meio" style="margin:0px 0px 0px 60px;" href="/cadastroIdosoAdm">Registro de Idosos</a>
-          </li>
-          <li class="nav-item item-menu">
-            <a class="nav-link-meio" style="margin:0px 0px 0px 60px;" href="/cadastroVoluntarioAdm">Registro de Voluntários</a>
-          </li>
+
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark sticky-top" id="ftco-navbar">
+    <div class="container">
+      <a href="/homer"><img class="navbar-brand" src="{{ asset('img/logo/abrigologo.png') }}"></a>
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto" >
+          <li class="nav-item active"><a href="/homer" style="color: white; font-weight: 600" class="nav-link">Ir para o Site</a></li>
+          <li class="nav-item"><a href="/publicacaoAdm" style="color: white; font-weight: 600" class="nav-link">Criar Publicação</a></li>
+          <li class="nav-item"><a href="/cadastroIdosoAdm" style="color: white; font-weight: 600" class="nav-link">Registro de Idosos</a></li>
+          <li class="nav-item"><a href="/cadastroVoluntarioAdm" style="color: white; font-weight: 600" class="nav-link">Registro de Voluntários</a></li>
         </ul>
       </div>
     </div>
-  </nav>
+</nav>
+
+
+
 
   @yield('contentAdm')
 
