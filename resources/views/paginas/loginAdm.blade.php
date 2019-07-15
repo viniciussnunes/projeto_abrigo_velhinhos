80512d40b5588b72eac55c52ce1968d15e08a8d3
Gustavo Pacheco <gustavo_gonzaga@estudante.sc.senai.br>
2019-07-15 14:33:37 -0300

    erro ao apertar em cancelar na pagina de cadastro consertado

--- resources/views/paginas/loginAdm.blade.php
+++ resources/views/paginas/loginAdm.blade.php
@@ -2,6 +2,7 @@
 
 @section('contentLogin')
 
+<div class="container" style="width: 350px">
 <div class="text-center login-card" style="display: initial">
   <form class="form-signin" action="/publicacaoAdm" style=" padding-top:60px">
     <img class="mb-4" src="{{asset('img/logo/abrigologo2.png')}}" alt="" width="72" height="72">
@@ -16,6 +17,7 @@
     <button class="btn btn-lg btn-primary btn-block" type="submit" style="background-color: #68c7e8">Entrar</button>
     <a class="btn btn-lg btn-secondary btn-block" href="/homer" style="background-color: #212529">Voltar para o Site</a>
   </form>
+</div>
 </div>
 
 @endsection
